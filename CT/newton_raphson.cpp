ce146f85051354ce7f5643d9fa935d362bd0470e
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-05-11 23:46:28 +0600

    CT

--- CT/newton_raphson.cpp
+++ CT/newton_raphson.cpp
@@ -3,60 +3,51 @@
 
 using namespace std;
 
-// Function definition
-double func(double x)
+// Define the function f(x) = cos(x) - xe^x
+double f(double x)
 {
-    return cos(x) - (x * exp(x));
+    return cos(x) - x * exp(x);
 }
 
-// Derivative of the function
-double derivative(double x)
+// Define the derivative f'(x)
+double f_prime(double x)
 {
-    return -exp(-x) - 10;
+    return -sin(x) - x * exp(x) + exp(x);
+}
+
+// Newton-Raphson method
+double newton_raphson(double initial_guess, double tolerance = 1e-9, int max_iterations = 100)
+{
+    double x = initial_guess;
+    for (int i = 0; i < max_iterations; ++i)
+    {
+        double fx = f(x);
+        double fpx = f_prime(x);
+        double x_new = x - fx / fpx;
+        if (abs(x_new - x) < tolerance)
+        {
+            return x_new;
+        }
+        x = x_new;
+    }
+    return NAN; // return NaN if the root is not found
 }
 
 int main()
 {
-    // Newton-Raphson method implementation
-    auto newtonRaphson = [](double x, double tol = 1e-6, int maxIter = 1000)
-    {
-        for (int iter = 0; iter < maxIter; ++iter)
-        {
-            double fx = func(x);
-            double fPrimeX = derivative(x);
+    // Initial guess in the interval [0, 1]
+    double initial_guess = 0.5;
 
-            if (fabs(fPrimeX) < 1e-6)
-            {
-                cout << "Derivative too close to zero." << endl;
-                return NAN;
-            }
-
-            double xNext = x - fx / fPrimeX;
-
-            if (fabs(xNext - x) < tol)
-            {
-                cout << "Converged after " << iter << " iterations." << endl;
-                return xNext;
-            }
-
-            x = xNext;
-        }
-
-        cout << "Did not converge after " << maxIter << " iterations." << endl;
-        return NAN;
-    };
-
-    double initialGuess = 1.0;
-    double root = newtonRaphson(initialGuess);
+    // Find the root using Newton-Raphson method
+    double root = newton_raphson(initial_guess);
 
     if (!isnan(root))
     {
-        cout << "Root found: " << root << endl;
-        cout << "Value of f(x) at the root: " << func(root) << endl;
+        cout << "The root of the equation cos(x) = xe^x in the interval [0, 1] is approximately: " << fixed << root << endl;
     }
     else
     {
-        cout << "Failed to find a root." << endl;
+        cout << "Unable to find root within the specified tolerance and maximum iterations." << endl;
     }
 
     return 0;
