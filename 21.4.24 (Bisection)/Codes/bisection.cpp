207aaea116b9a2bf893d70ee7d4ac488cc2993a3
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-05-14 00:22:40 +0600

    Screenshots Added

--- 21.4.24 (Bisection)/Codes/bisection.cpp
+++ 21.4.24 (Bisection)/Codes/bisection.cpp
@@ -6,43 +6,49 @@ author = Hafiz_Sakib;
 long long Id = 0222210005101118;
 
 */
+
 #include <bits/stdc++.h>
 using namespace std;
 
 #define setp(n) fixed << setprecision(n)
-const double error = 1e-3;
+const double error = 1e-4;
 
 double f(double x)
 {
-    return ((x * x * x) - (2 * x * x) - 4);
+    // return ((x * x * x) - (2 * x * x) - 4);
+    // return ((sin(x) * sin(x)) - (x * x) + 1);
+    // return ((x * x) + log(x) - 2);
+    return ((sin(x) * sin(x)) - (x * x) + 1);
 }
 
 int main()
 {
     double x = 2.0, y = 3.0, mid = 0.00;
-    
-    if (f(x) * f(y) > 0)
+    while (1)
     {
-        cout << "The root doesn't lies between the given interval" << endl;
-    }
-    else
-    {
-        while (abs(x - y) > error)
+        cout << "Enter the interval [a, b]: ";
+        cin >> x >> y;
+        if (f(x) * f(y) < 0)
         {
-            mid = (x + y) / 2 * 1.00;
-            double Xn = f(mid);
-            if (Xn == 0)
-            {
-                break;
-            }
-            else if (Xn > 0)
-            {
-                y = mid;
-            }
-            else
-            {
-                x = mid;
-            }
+            break;
+        }
+    }
+    while (abs(x - y) > error)
+    {
+        mid = (x + y) / 2.00;
+        cout << "Mid Value = " << mid << endl;
+        cout << "f(mid) Value = " << f(mid) << endl;
+        if (f(mid) == 0)
+        {
+            break;
+        }
+        else if (f(mid) * f(x) < 0)
+        {
+            y = mid;
+        }
+        else
+        {
+            x = mid;
         }
     }
     cout << "The Root of The Equation is : " << setp(3) << mid << endl;
